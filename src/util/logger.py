379ff09345015dcb0a4f2ed763c10058989ab8e7
Frederik Polachowski <frederik.polachowski@tu-dortmund.de>
2025-01-03 19:13:19 +0000

    added logger

---
+++ src/util/logger.py
@@ -0,0 +1,44 @@
+import wandb
+from dataclasses import dataclass
+
+@dataclass
+class TrainingConfig():
+    learning_rate: float
+    epochs: int
+    batch_size: int
+    early_stoping_step: int
+    loss_accumulation_step: int
+    scheduler_steps: list[int]
+    scheduler_decay: float
+    model_save_interval: int
+    experiment_folder: str
+    debug_flag: bool
+    files: list[str]
+
+class WandbLogger():
+    def __init__(self, name, options: TrainingConfig) -> None:
+        self.options = options
+        wandb.login()
+        self.run = wandb.init(
+            # Set the project where this run will be logged
+            project=name,
+            # Track hyperparameters and run metadata
+            config={
+                "epochs": options.epochs,
+                "learning_rate": options.learning_rate,
+                "scheduler_steps": options.scheduler_steps,
+                "scheduler_decay": options.scheduler_decay,
+                "batch_size": options.batch_size,
+                "early_stoping_step": options.early_stoping_step
+            },
+            settings=wandb.Settings(disable_git=True)
+        )
+        
+        for f in options.files:
+            wandb.save(f)
+        
+        
+            
+    def log(self, value_dict: dict[str, float]):
+        for name in value_dict:
+            self.run.log({name: value_dict[name]})
